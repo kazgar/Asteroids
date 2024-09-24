ad32b69446a19aa035193abf6606466c8894fb23
kazgar <kazimierz.garstecki@gmail.com>
2024-09-24 20:41:41 +0200

    added asteroid.py and asteroidfield.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -10,7 +12,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
     dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
