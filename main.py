2fc0c1ac06ca682acb620acc08b5b92a39153199
kazgar <kazimierz.garstecki@gmail.com>
2024-09-24 19:01:06 +0200

    added rotation to player.py and implemented in main.py

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
+        
+
         
 
 
